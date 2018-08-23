a977120bcae445d19fe25863b609098083d8dcf9
csekhar193 <chandrasekhar193.ece@gmail.com>
2018-08-23 15:46:49 +0530

    Add mustache.js to messages template.

--- public/js/index.js
+++ public/js/index.js
@@ -5,21 +5,26 @@ socket.on('connect', function () {
 });
 
 socket.on('newMessage', function (newMessage) {
-	var formattedTime = moment(newMessage.createdAt).format('h:mm a');
-	let li = jQuery('<li></li>');
-	li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
-	jQuery('#messages').append(li);
+	let formattedTime = moment(newMessage.createdAt).format('h:mm a');
+	let template = jQuery('#message-template').html();
+	let html = Mustache.render(template, {
+		from : newMessage.from,
+		text : newMessage.text, 
+		createdAt : formattedTime
+	});
+	jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (location) {
-	var formattedTime = moment(location.createdAt).format('h:mm a');
-	let li = jQuery('<li></li>');
-	let a = jQuery('<a target="_blank">My Current Location.</a>');
-	li.text(`${location.from} ${formattedTime}: `);
-	a.attr('href', location.link);
-	li.append(a);
-	jQuery('#messages').append(li);
-})
+	let formattedTime = moment(location.createdAt).format('h:mm a');
+	let template = jQuery('#location-message-template').html();
+	let html = Mustache.render(template, {
+		from : location.from,
+		url : location.link,
+		createdAt : formattedTime	
+	});
+	jQuery('#messages').append(html);
+});
 
 socket.on('disconnect', function () {
 	console.log('Disconnected from server.')
